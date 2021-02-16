3759013ca040805d21b4ebe88b67c5cd313c90e9
Iacopo <iacopo.ghila@gmail.com>
2021-02-16 17:10:32 +0100

    show and index

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,15 +1,27 @@
 @extends('layouts.main')
 
+@section('header')
+    <div class="container mt-5 mb-5">
+        <h1 class="text-center">Blog</h1>
+    </div>
+@endsection
 
 @section('content')
-    @foreach ($articles as $article)
-        <div>
-            <h3>{{ $article["title"] }}</h3>
-            <p>{{ $article["subtitle"] }}</p>
-            <span>{{ $article["author"] }}</span>
-            <a href="{{ route('articles.show', $article) }}">Vai all'articolo</a>
-        </div>
-
-
-    @endforeach
+    <div class="container">
+        @foreach ($articles as $article)
+            <div class="card" style="width: 37rem;">
+                <div class="card-body">
+                <h3 class="card-title">{{ $article["title"] }}</h3>
+                <h6 class="card-subtitle mb-2 text-muted">{{ $article["subtitle"] }}</h6>
+                <p class="card-text">{{ Str::substr($article["text"], 0, 50) }}...</p>
+                <a href="{{ route('articles.show', $article) }}">Vai all'articolo</a>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
+
+
+
+
+
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,10 +1,19 @@
 @extends('layouts.main')
 
+@section('header')
+    <div class="container mt-5">
+        <h1 class="text-center">{{ $article->title }}</h1>
+    </div>
+@endsection
 
 @section('content')
-    <h2>{{ $article->title }}</h2>
-    <p>{{ $article->subtitle }}</p>
-    <article>{{ $article->text }}</article>
-    <span>{{ $article->author }}</span>
-    <span>{{ $article->publication_date }}</span>
+    <div class="container">  
+        <p class="text-center">{{ $article->subtitle }}</p>
+        <article>{{ $article->text }}</article>
+        <div class="d-flex justify-content-between mt">
+            <span>{{ $article->author }}</span>
+            <span class="text-right">{{ $article->publication_date }}</span>
+        </div>
+    </div>
+
 @endsection
